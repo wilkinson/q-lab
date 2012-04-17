4cb2796418e72ab2a40e277d57d7189a047d494a
Sean Wilkinson <sean@mathbiol.org>
2012-04-17 16:10:29 -0500

    Bound to the wrong hostname?

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var http, port;
 
 http = require('http');
 
-port = process.env.PORT || 3000;
+port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello world!\n');
-}).listen(port, '127.0.0.1');
+}).listen(port, '0.0.0.0');
 
-console.log('Server running at http://127.0.0.1:' + port + ' ...');
+//console.log('Server running at http://127.0.0.1:' + port + ' ...');
 
