0b51f871bcaf5bd2b4b5c42a75b2953bb2489c8e
Sean Wilkinson <sean@mathbiol.org>
2012-07-25 20:48:49 -0500

    Added Google Analytics

--- public_html/main.js
+++ public_html/main.js
@@ -3,12 +3,12 @@
 //- main.js ~~
 //                                                      ~~ (c) SRW, 25 Jul 2012
 
-(function () {
+(function (global) {
     'use strict';
 
  // Pragmas
- 
-    /*jslint indent: 4, maxlen: 80 */
+
+    /*jslint indent: 4, maxlen: 80, nomen: true */
 
  // Prerequisites
 
@@ -26,7 +26,22 @@
 
  // Out-of-scope definitions
 
- // Invocations
+    if (global.hasOwnProperty('_gaq') === false) {
+     // This creates a global-scope reference for use with Google Analytics.
+        global._gaq = [];
+    }
+
+ // Google Analytics
+
+    global._gaq.push(['_setAccount', 'UA-33661349-1']);
+    global._gaq.push(['_setDomainName', 'qmachine.org']);
+    global._gaq.push(['_trackPageview']);
+
+    if (global.location.protocol === 'https:') {
+        Q.lib('https://ssl.google-analytics.com/ga.js');
+    } else {
+        Q.lib('http://www.google-analytics.com/ga.js');
+    }
 
     Q.lib('//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
 
@@ -34,6 +49,15 @@
 
     return;
 
-}());
+}(Function.prototype.call.call(function (that) {
+    'use strict';
+ // See the bottom of "quanah.js" for documentation.
+    /*jslint indent: 4, maxlen: 80 */
+    /*global global: true */
+    if (this === null) {
+        return (typeof global === 'object') ? global : that;
+    }
+    return (typeof this.global === 'object') ? this.global : this;
+}, null, this)));
 
 //- vim:set syntax=javascript:
